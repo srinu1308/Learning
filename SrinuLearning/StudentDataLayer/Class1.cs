cda616856fd133ae0aec0577cd5d0d1617685575
Srinu Vadapalli <Srv022@diyarme.com>
2019-07-27 12:36:26 +0300

    adding files

--- SrinuLearning/StudentDataLayer/Class1.cs
+++ SrinuLearning/StudentDataLayer/Class1.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
 
 namespace StudentDataLayer
 {
     public class Class1
     {
+
+        public void GetStudents()
+        {
+            SqlConnection conn = null;
+            string connectionString = ConfigurationManager
+                .ConnectionStrings["studentmdfconnection"].ConnectionString;
+
+            using (conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "select * from student";
+                SqlDataReader reader=cmd.ExecuteReader();
+
+                while(reader.Read())
+                {
+                    string name = reader.GetString(1);
+                }
+            }
+        }
     }
 }
---
+++ SrinuLearning/StudentDataLayerTesting/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudentDataLayer;
+namespace StudentDataLayerTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Class1 obj1 = new Class1();
+            obj1.GetStudents();
+        }
+    }
+}
