2fd1b1755eaae33a8d6b52ef7c4d646ed9e3e135
svadapalli <srinu.vadapalli664@gmail.com>
2019-09-15 22:47:45 +0530

    added projects for formbased and owin auth

---
+++ SrinuLearning/ASPMVCBasicAuthCheck/Global.asax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace ASPMVCBasicAuthCheck
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_AuthenticateRequest()
+        {
+           if( Request.Url.AbsolutePath.ToLower()
+                .Contains("contact"))
+            {
+                GenericIdentity identity = new GenericIdentity("testuser");
+                GenericPrincipal pricipal = new GenericPrincipal(identity, null);
+                this.Context.User = pricipal;
+                
+            }            
+        }
+    }
+}
---
+++ SrinuLearning/ASPMVCOnlyOwinAuth/Controllers/UserLoginController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Host.SystemWeb;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace ASPMVCOnlyOwinAuth.Controllers
+{
+    [Authorize]
+    public class UserLoginController : Controller
+    {
+        // GET: UserLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        [ActionName("Login")]
+        public ActionResult Login_post(string txtUserName,string txtUserPassword)
+        {
+            string userName=System.Configuration.ConfigurationManager.AppSettings["UserName"];
+            string password = System.Configuration.ConfigurationManager.AppSettings["UserPassword"];
+
+            if(txtUserName.Equals(userName) && txtUserPassword.Equals(password))
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name.ToString(), userName) };
+                var identity = new ClaimsIdentity(claims, "ApplicationCookie");
+
+                var context = Request.GetOwinContext();
+                var authManager = context.Authentication;
+                authManager.SignIn(
+                    new Microsoft.Owin.Security.AuthenticationProperties()
+                    {
+                        IsPersistent =false,
+                        ExpiresUtc =new DateTimeOffset(DateTime.UtcNow.AddMinutes(1))
+                    }, 
+                    identity);
+
+                return RedirectToAction("Index", "Home",null);
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public ActionResult LogOut()
+        {
+            var owincontext=Request.GetOwinContext();
+            var authentication=owincontext.Authentication;
+            authentication.SignOut("ApplicationCookie");
+            return RedirectToAction("Login", "UserLogin", null);
+        }
+    }
+}
